da0c9c102abbb7faa72e7cd118399ded2a45a8c5
ovtsa <nscottjobe@gmail.com>
2024-11-20 12:06:48 -0600

    basic about me page

--- src/features/AboutPage/AboutPage.js
+++ src/features/AboutPage/AboutPage.js
@@ -1,3 +1,23 @@
+import { Link } from "react-router-dom";
+import "./AboutPage.css";
+
 export default function AboutPage() {
-    return <h1>About meeee :3</h1>
+    const linkedinLink = "https://www.linkedin.com/in/nathan-jobe/";
+    const githubLink = "https://github.com/ovtsa";
+
+    return (
+        <div className="about-page-container">
+            <h1>Nathan Jobe</h1>
+            <p>
+                Software engineer relocating from Chicago to Pittsburgh. I have a
+                passion for logistics and data, and creating applications that make
+                complicated systems simple and accessible for end users.
+            </p>
+            <p>If you would like to get in touch, feel free to reach out to me via my socials below:</p>
+            <ul>
+                <li><Link className="styled-link" to={linkedinLink} target="_blank">LinkedIn</Link></li>
+                <li><Link className="styled-link" to={githubLink} target="_blank">GitHub</Link></li>
+            </ul>
+        </div>
+    )
 }
